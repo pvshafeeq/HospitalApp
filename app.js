99dba305902b6845e9d1e2626dbaeca36a810865
pvshafeeq <61697146+pvshafeeq@users.noreply.github.com>
2022-12-16 20:47:05 +0300

    Hospital App

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = new express();
+app.use(express.json());
+const data = require('./dataset.json');
+const fs = require('fs');
+
+
+app.get('/hospital', (req, res) => {
+    res.send(data);
+}
+);
+
+app.post('/hospital', (req, res) => {
+    data.push(req.body);
+    fs.writeFile('dataset.json', JSON.stringify(data), (err, resp) => {
+        if (err) {
+            res.send("Error: Failed to write data");
+        }
+        else {
+            res.send("Data written successfully");
+        }
+    });
+});
+
+
+app.put('/hospital/:name', (req, res) => {
+    let name = req.params.name;
+    data.forEach((item) => {
+        if (item.hospitalName == name) {
+            item.patientCount = req.body.patientCount;
+            item.Location = req.body.Location;
+        }
+    })
+    fs.writeFile('dataset.json', JSON.stringify(data), (err, resp) => {
+        if (err) {
+            res.send("Error: Failed to update data");
+        }
+        else {
+            res.send("Data updated successfully");
+        }
+    });
+});
+
+app.delete('/hospital/:name', (req, res) => {
+    let name = req.params.name;
+    let value = data.filter((item) => item.hospitalName != name);
+    fs.writeFile('dataset.json', JSON.stringify(value), (err, resp) => {
+        if (err) {
+            res.send("Error: Failed to delete data");
+        }
+        else {
+            res.send("Data deleted successfully");
+        }
+    });
+});
+
+//listen to port
+app.listen(3000, () => {
+    console.log('server is listening to 3000');
+    console.log(data);
+})
